51ffeb289ac26c1d22381fd7c43b3a1392d105b3
birhanukassa <ricardogebremichael@gmail.com>
2024-05-19 04:30:12 -0500

    Implemented dynamic type acceptance in InputHandler, enhanced TypedNameValue class with null checks, serialization, and documentation, and made significant improvements to prioritization integration.

--- taskmanager-core/src/main/java/com/github/birhanukassa/taskmanagement/commands/TaskCommand.java
+++ taskmanager-core/src/main/java/com/github/birhanukassa/taskmanagement/commands/TaskCommand.java
@@ -1,10 +1,7 @@
 package com.github.birhanukassa.taskmanagement.commands;
 
 import com.github.birhanukassa.taskmanagement.models.Task;
-import com.github.birhanukassa.taskmanagement.models.TypedNameValue;
-
-import java.util.List;
 
 interface TaskCommand<V> {
-    TypedNameValue<V> execute(List<Task> tasks);
+    void execute(Task task);
 }
