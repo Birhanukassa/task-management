07a57615c7657b7050bf19679601c5f67632124f
birhanu kassa <artbirhanu@gmail.com>
2024-09-02 14:50:42 -0500

    Update App.java
    remove  comment

--- src/main/java/com/github/birhanukassa/taskmanagement/App.java
+++ src/main/java/com/github/birhanukassa/taskmanagement/App.java
@@ -1,6 +1,5 @@
 package com.github.birhanukassa.taskmanagement;
 
-// Importing necessary classes and packages
 import com.github.birhanukassa.taskmanagement.commands.*;
 import com.github.birhanukassa.taskmanagement.display.*;
 import com.github.birhanukassa.taskmanagement.models.*;
